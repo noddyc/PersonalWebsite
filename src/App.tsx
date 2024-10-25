97b5344f9977372d28d4f5e652c41da87f15a0c2
jianhe2git <59978308+noddyc@users.noreply.github.com>
2024-10-25 10:34:40 -0400

    1

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ const App = () => {
         <div className="dAbout">
           <About />
         </div>
-        <div>
+        <div className="dParagraph">
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard
             dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
