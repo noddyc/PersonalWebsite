a9bb41e02edbbb12d8e841d10808358d0e5ff41f
jianhe2git <59978308+noddyc@users.noreply.github.com>
2024-10-25 10:58:31 -0400

    1

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -26,7 +26,7 @@ const Portfolio = () => {
       } else {
         setDD('none');
       }
-    }, 500);
+    }, 300);
   }, [clicked]);
 
   return (
@@ -41,27 +41,32 @@ const Portfolio = () => {
       >
         <h2>PR</h2>
         {clicked % 2 === 0 ? (
-          <div className="Portfolio__div__list Portfolio__div" />
-        ) : (
           <div className="Portfolio__div__list">
             <div className="Portfolio__div Portfolio__div--Size" />
           </div>
+        ) : (
+          <div className="Portfolio__div__list Portfolio__div" />
         )}
         <h2>JECTS</h2>
       </a>
 
-      <div className="Portfolio__imageProjects">
+      {/* <div className="Portfolio__imageProjects">
         <ImageProject direction="left" borderDirection="ImageProject__link--BorderOnLeft" />
         <ImageProject direction="right" borderDirection="ImageProject__link--BorderOnRight" />
         <ImageProject direction="left" borderDirection="ImageProject__link--BorderOnTop" />
-      </div>
-      {clicked !== 0 && clicked % 2 === 0 && (
+      </div> */}
+      {clicked === 0 && (
+        <div>
+          <Display />
+        </div>
+      )}
+      {clicked !== 0 && clicked % 2 !== 0 && (
         <div className="Portfolio__children--Shrink" style={{ display: dd }}>
           <Display />
         </div>
       )}
 
-      {clicked !== 0 && clicked % 2 !== 0 && (
+      {clicked !== 0 && clicked % 2 === 0 && (
         <div className="Portfolio__children--Expand">
           <Display />
         </div>
